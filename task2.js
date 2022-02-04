e2e889508d19ad00bfec5e696fd194219d206ee4
tinyteabird <87604794+tinyteabird@users.noreply.github.com>
2022-02-04 11:46:17 -0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,23 +1,5 @@
 // Given an integer x, return true if x is palindrome integer.
 
 var isPalindrome = function(x) {
-    let y;
-    let array = [];
-    let z;
-    if (x<0) {
-        return false;
-    } else if (x===0) {
-        return true
-    } else {
-    x = x.toString();
-    y = x;
-    for (let i=0; i<y.length; i++) {
-        z = y.charAt(i);
-        array.unshift(z);
-    } 
-    y = array.join("");
-    };
-    if (x === y) return true;
-    else return false
+return x == x.toString().split("").reverse().join("")
 };
-
